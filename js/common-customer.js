79f5c6a42d2d575094bcaf1ed3e910535707f03b
fli <905421889@qq.com>
2016-10-02 10:26:40 +0800

    Site updated: 2016-10-02 10:26:36

---
+++ js/common-customer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by feili on 2016/10/2.
+ */
+
+/**
+ * code block fold and unfold with dblclick event
+ */
+document.body.onload=function(){var curCode=document.getElementsByClassName('highlight');for(var i=0;i<curcode.length;i++){var obj="curCode[i];obj.ondblclick=function(){var" objclass="this.className;if(objClass.indexOf('fold')">-1){this.className=objClass.replace('fold','')}else{this.className=objClass+' fold'}}}};</curcode.length;i++){var>
