3ed52a00244cb2ee54ab10ffd647c04fe9dfc59b
fli <905421889@qq.com>
2016-10-02 11:04:03 +0800

    Site updated: 2016-10-02 11:03:41

---
+++ js/src/fli-customer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by feili on 2016/10/2.
+ */
+
+/**
+ * code block fold and unfold with dblclick event
+ */
+document.body.onload=function(){var b=document.getElementsByClassName('highlight');for(var a=0;a<b.length;a++){var c=b[a];c.ondblclick=function(){var d=this.className;if(d.indexOf('fold')>-1){this.className=d.replace('fold','');}else{this.className=d+' fold';}}}};
