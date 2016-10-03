5c3eacab0d7b60b8276d5fdea76bfeb2ece8da6b
fli <905421889@qq.com>
2016-10-03 16:29:44 +0800

    Site updated: 2016-10-03 16:29:39

--- js/src/fli-customer.js
+++ js/src/fli-customer.js
@@ -7,15 +7,17 @@
  */
 document.body.onload = function(){
   var curCode = document.getElementsByClassName("highlight");
-  for(var i=0; i<curCode.length; i++){
-    var obj = curCode[i];
-    obj.ondblclick = function(){
-      var objClass = this.className;
-      if(objClass.indexOf("fold")>-1){
-        this.className=objClass.replace("fold","");
-      }else{
-        this.className=objClass+" fold";
-      }
+}
+for(var i=0; i<curCode.length; i++){
+  var obj = curCode[i];
+  //add hightlight code class "hightight-customer"
+  obj.className +=" hightight-customer";
+  obj.ondblclick = function(){
+    var objClass = this.className;
+    if(objClass.indexOf("fold")>-1){
+      this.className=objClass.replace("fold","");
+    }else{
+      this.className=objClass+" fold";
     }
   }
 }
